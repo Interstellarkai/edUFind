ad29cd6b34ec70cf7992a22d7f059f58da1d890e
KaiSheng Lim <limkaisheng.work@gmail.com>
2022-02-20 03:29:12 +0800

    Completed static routes for school entities, partial filter, and indexing for name. Require follow up on filter, addition of other school DAO, and logic flow

---
+++ backend/api/schools.controller.js
@@ -0,0 +1,66 @@
+// School Controller Class 
+import SchoolsDAO from "../dao/schoolsDAO.js"
+
+export default class SchoolsController {
+
+    static async apiGetSchools(req, res, next) {
+        // api call is called through a url --> query string (specify certain parameters)
+        // check if the query in the url exists, then parse it to an integer. Else default is 20
+        const schoolsPerPage = req.query.schoolsPerPage ? parseInt(req.query.schoolsPerPage, 10) : 20
+        const page = req.query.page ? parseInt(req.query.page, 10) : 0
+        
+        let filters = {} // Filter starts emppty
+        // if zone_code is in the query string, then the zone_code is set to the query string
+        if (req.query.zone_code) {
+            filters.zone_code = req.query.zone_code
+        } else if (req.query.postal_code) {
+            filters.postal_code = req.query.postal_code
+        } else if (req.query.school_name) {
+            filters.school_name = req.query.school_name
+        }
+        
+        // call the getSchools 
+        const { schoolsList, totalNumSchools } = await SchoolsDAO.getSchools({
+            filters,
+            page,
+            schoolsPerPage,
+        })
+
+        // response when the api url is called 
+        let response = {
+            schools: schoolsList,
+            page: page,
+            filters: filters,
+            entries_per_page: schoolsPerPage,
+            total_results: totalNumSchools,
+        }
+        res.json(response) // send a json response to whoever made the request
+    }
+
+    static async apiGetSchoolById(req, res, next) {
+        try {
+            let id = req.params.id || {}
+            let school = await SchoolsDAO.getSchoolById(id)
+            if (!school) {
+                res.status(404).json({ errpr: "Not found" })
+                return
+            }
+            res.json(school)
+        } catch (e) {
+            console.log(`api, ${e}`)
+            res.status(500).json({ error: e })
+        }
+    }
+
+    static async apiGetSchoolZoneCode(req, res, next) {
+        // get zonecode if not error
+        try {
+            let zonecode = await SchoolsDAO.getZoneCode()
+            res.json(zonecode)
+        } catch (e) {
+            console.log(`api, ${e}`)
+            res.status(500).json({ error: e })
+        }
+    }
+
+}
---
+++ backend/dao/schoolsDAO.js
@@ -0,0 +1,133 @@
+// DAO - Data Access Object 
+import mongodb from "mongodb"
+const ObjectId = mongodb.ObjectId
+let schools // store reference to db 
+
+
+// export class with several async methods
+export default class SchoolsDAO {
+    // connect to db initially (as our server starts)
+    static async injectDB(conn) {
+        // reference is filled 
+        if (schools) {
+            return
+        }
+        // if reference is not filled, we will try to connect with a specific reference in our DB 
+        try {
+            schools = await conn.db(process.env.SCHOOLREVIEWS_NS).collection("general_information_of_schools")
+        } catch (e) {
+            console.error(
+                `Unable to establish a collection handle in schoolsDAO: ${e}`
+            )
+        }
+    }
+
+    // get a list of schools in the DB 
+    static async getSchools({
+        // options 
+        filters = null,
+        page = 0,
+        schoolsPerPage = 20,
+    } = {}) {
+        // query
+        let query
+        // 3 different filters 
+        if (filters) {
+            if ("school_name" in filters) {
+                query = { $text: { $search: filters["school_name"] } } // text search, search any word in that text
+            }
+            else if ("zone_code" in filters) {
+                query = { "zone_code": { $eq: filters["zone_code"] } } // if zone_code in db equals to the zone_code passed in, search for zone_code
+            }
+            else if ("postal_code" in filters) {
+                query = { "postal_code": { $eq: filters["postal_code"] } }
+            }
+        }
+
+        let cursor
+
+        try {
+            // find all the schools from the database that go along with the query that was passed in
+            cursor = await schools
+                .find(query)
+        } catch (e) {
+            console.error(`Unable to issue find command, ${e}`)
+            return { schoolsList: [], totalNumSchools: 0 }
+        }
+
+        const displayCursor = cursor.limit(schoolsPerPage).skip(schoolsPerPage * page) // get to a specific page
+
+        try {
+            const schoolsList = await displayCursor.toArray()
+            const totalNumSchools = await schools.countDocuments(query)
+
+            return { schoolsList, totalNumSchools }
+        } catch (e) {
+            console.error(
+                `Unable to convert cursor to array or problem counting documents, ${e}`
+            )
+            return { schoolsList: [], totalNumSchools: 0 }
+        }
+    }
+
+    // get reviews and put it into school
+    static async getSchoolById(id) {
+        try {
+            // create a pipeling (mongo db) to help match different collections together
+            const pipeline = [
+                {
+                    $match: {
+                        _id: new ObjectId(id),
+                    },
+                },
+                {
+                    // lookup other items to add to the results 
+                    $lookup: {
+                        from: "reviews",
+                        let: {
+                            id: "$_id",
+                        },
+                        pipeline: [
+                            {
+                                // find all the reviews that match the school id
+                                $match: {
+                                    $expr: {
+                                        $eq: ["$school_id", "$$id"],
+                                    },
+                                },
+                            },
+                            {
+                                $sort: {
+                                    date: -1,
+                                },
+                            },
+                        ],
+                        as: "reviews", // new field
+                    },
+                },
+                {
+                    $addFields: {
+                        reviews: "$reviews",
+                    },
+                },
+            ]
+            // aggregate the pipeline means to combine everything together
+            // return the next item 
+            return await schools.aggregate(pipeline).next()
+        } catch (e) {
+            console.error(`Something went wrong in getSchoolByID: ${e}`)
+            throw e
+        }
+    }
+
+    static async getZoneCode() {
+        let zonecode = []
+        try {
+            zonecode = await schools.distinct("zone_code")
+            return zonecode
+        } catch (e) {
+            console.error(`Unable to get zonecode, ${e}`)
+            return zonecode
+        }
+    }
+}
