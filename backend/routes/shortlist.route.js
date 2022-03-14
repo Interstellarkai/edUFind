7033f44d7924905d1b88e5c713438fec07886d09
KaiSheng Lim <limkaisheng.work@gmail.com>
2022-03-14 22:15:52 +0800

    shortlist with token

--- backend/routes/shortlist.route.js
+++ backend/routes/shortlist.route.js
@@ -4,16 +4,9 @@ import VerifyJWTToken from '../middleware/verifyJWT.js';
 
 const router = express.Router()
 
-<<<<<<< Updated upstream
-router.route('/').post(ShortlistCtrl.addShortlist)
-router.route('/id/:id/editShortlist').put(ShortlistCtrl.editShortlist)
-router.route('/id/:id/deleteShortlisted').delete(ShortlistCtrl.removeShortlistItem) 
-router.route('/id/:id/viewShortlist').get(ShortlistCtrl.getShortlisted) 
-=======
 router.post('/:id', VerifyJWTToken, ShortlistCtrl.addToShortlist)
 router.get('/:id/viewShortlist', VerifyJWTToken, ShortlistCtrl.getShortlisted)
 router.put('/:id/editShortlist', VerifyJWTToken, ShortlistCtrl.editShortlist)
 router.delete('/:id/deleteShortlisted', VerifyJWTToken, ShortlistCtrl.removeShortlistItem)
->>>>>>> Stashed changes
 
 export default router
