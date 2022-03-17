a21aff6ac7bc596f0430e91d61aa058a63b1c2b9
ling-yin <64789716+ling-yin@users.noreply.github.com>
2022-03-17 17:28:08 +0800

    updates to shortlist

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import express, { application } from "express";
 import cors from "cors";
 import schools from "./routes/schools.route.js"; // Routes for school related functions
 import users from "./routes/user.route.js"; // Routes for user related functions
+import shortlist from "./routes/shortlist.route.js"
 import dotenv from "dotenv"; // access env var
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken"; // Token Authentication
@@ -18,6 +19,7 @@ app.use(express.json()); // server can accept json in the body of a request
 // initial routes
 app.use("/schools", schools);
 app.use("/users", users);
+app.use("/shortlist", shortlist);
 app.use("*", (req, res) => res.status(404).json({ error: "not found" })); // * means wild card, a route that is not in our route file, then we return 404 page not found
 
 // export app as a module --> import the file that accesses the db
