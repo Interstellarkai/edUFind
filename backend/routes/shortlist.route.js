7f74b3e32147bfca918f837c68d77e7e898dfeaf
KaiSheng Lim <limkaisheng.work@gmail.com>
2022-03-18 15:09:06 +0800

    Change the shortlist param to body

--- backend/routes/shortlist.route.js
+++ backend/routes/shortlist.route.js
@@ -1,12 +1,12 @@
-import express from "express"
-import ShortlistCtrl from "../controllers/shortlist.controller.js"
-import VerifyJWTToken from '../middleware/verifyJWT.js';
+import express from "express";
+import ShortlistCtrl from "../controllers/shortlist.controller.js";
+import VerifyJWTToken from "../middleware/verifyJWT.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/:id', VerifyJWTToken, ShortlistCtrl.addShortlist)
-router.get('/:id/viewShortlist', VerifyJWTToken, ShortlistCtrl.getShortlisted)
-router.put('/:id/editShortlist', VerifyJWTToken, ShortlistCtrl.editShortlist)
-router.delete('/:id/deleteShortlisted', VerifyJWTToken, ShortlistCtrl.removeShortlistItem)
+router.post("/", VerifyJWTToken, ShortlistCtrl.addShortlist);
+router.get("/:id/viewShortlist", VerifyJWTToken, ShortlistCtrl.getShortlisted);
+router.put("/:id/editShortlist", VerifyJWTToken, ShortlistCtrl.editShortlist);
+router.delete("/:id/deleteShortlisted", VerifyJWTToken, ShortlistCtrl.removeShortlistItem);
 
-export default router
+export default router;
