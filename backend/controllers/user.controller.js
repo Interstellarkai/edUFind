74f16df099484e3f4f2ee5297fbea416e15f7ae6
Andre Lim <Andre_Lim99@outlook.com>
2022-03-18 11:28:47 +0800

    Trying some stuff

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -4,236 +4,227 @@ import UserAuthDAO from "../dao/userAuthDAO.js";
 // import { Request, Response } from 'express'
 
 export default class UserController {
-	static async registerUser(req, res, next) {
-		const username = req.body.username;
-		const password = req.body.password;
-		const email = req.body.email;
-		const gender = req.body.gender;
-		const region = req.body.region;
-		const mtl = req.body.motherTongueLanguage;
-		const eduLevel = req.body.educationLevel;
-		const interests = req.body.ccaInterests;
+  static async registerUser(req, res, next) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+    const gender = req.body.gender;
+    const region = req.body.region;
+    const mtl = req.body.motherTongueLanguage;
+    const eduLevel = req.body.educationLevel;
+    const interests = req.body.ccaInterests;
 
-		// Compulsory fields are not completed
-		if (!username || !email || !password) {
-			// return res.json({ error: "Add all data" })
-			return res.json({
-				success: false,
-				errorType: "RegisterFieldsEmpty",
-				message:
-					"Compulsory fields (Username, Email, Password) are not completed",
-			});
-		}
+    // Compulsory fields are not completed
+    if (!username || !email || !password) {
+      // return res.json({ error: "Add all data" })
+      return res.json({
+        success: false,
+        errorType: "RegisterFieldsEmpty",
+        message:
+          "Compulsory fields (Username, Email, Password) are not completed",
+      });
+    }
 
-		// Check validity of email
-		const emailRegex =
-			"[a-zA-Z0-9_\\.\\+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-\\.]+";
-		if (!email.match(emailRegex)) {
-			console.error(`Invalid email: ${email}`);
-			return res.json({
-				success: false,
-				errorType: "RegisterEmailRegex",
-				message: "Email must be in name@email.com format",
-			});
-		} else if (email.length > 300) {
-			console.error(`Invalid email: ${email}`);
-			return res.json({
-				success: false,
-				errorType: "RegisterEmailLength",
-				message: "Email is too long",
-			});
-		}
+    // Check validity of email
+    const emailRegex = "[a-zA-Z0-9_\\.\\+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-\\.]+";
+    if (!email.match(emailRegex)) {
+      console.error(`Invalid email: ${email}`);
+      return res.json({
+        success: false,
+        errorType: "RegisterEmailRegex",
+        message: "Email must be in name@email.com format",
+      });
+    } else if (email.length > 300) {
+      console.error(`Invalid email: ${email}`);
+      return res.json({
+        success: false,
+        errorType: "RegisterEmailLength",
+        message: "Email is too long",
+      });
+    }
 
-		// Check length of password
-		if (password.length < 6 || password.lenth > 50) {
-			console.error(`Invalid password length: ${password}`);
-			return res.json({
-				success: false,
-				errorType: "RegisterPasswordLength",
-				message: "Password should be within length of 6 - 50.",
-			});
-		}
-		try {
-			const status = await UserServices.Register(
-				username,
-				email,
-				password,
-				gender,
-				region,
-				mtl,
-				eduLevel,
-				interests,
-				req,
-				res,
-				next
-			);
-			return res.json(status);
-		} catch (err) {
-			console.error(`AuthController: Register: ${err}`);
-			return res.json({
-				success: false,
-				errorType: "RegisterCatchBlock",
-				message: `An error occured while trying to create account ${err}`,
-			});
-		}
-	}
+    // Check length of password
+    if (password.length < 6 || password.lenth > 50) {
+      console.error(`Invalid password length: ${password}`);
+      return res.json({
+        success: false,
+        errorType: "RegisterPasswordLength",
+        message: "Password should be within length of 6 - 50.",
+      });
+    }
+    try {
+      const status = await UserServices.Register(
+        username,
+        email,
+        password,
+        gender,
+        region,
+        mtl,
+        eduLevel,
+        interests,
+        req,
+        res,
+        next
+      );
+      return res.json(status);
+    } catch (err) {
+      console.error(`AuthController: Register: ${err}`);
+      return res.json({
+        success: false,
+        errorType: "RegisterCatchBlock",
+        message: `An error occured while trying to create account ${err}`,
+      });
+    }
+  }
 
-	static async userLogin(req, res) {
-		const { email, password } = req.body;
+  static async userLogin(req, res) {
+    const { email, password } = req.body;
 
-		const emailRegex =
-			"[a-zA-Z0-9_\\.\\+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-\\.]+";
-		if (!email.match(emailRegex)) {
-			console.error(
-				`AuthController: Login: Invalid email string: ${email}`
-			);
-			return res.json({
-				success: false,
-				errorType: "LoginEmailRegex",
-				message: "Email must be in name@email.com format",
-			});
-		} else if (email.length > 300) {
-			console.error(
-				`AuthController: Login: Invalid email string: ${email}`
-			);
-			return res.json({
-				success: false,
-				errorType: "LoginEmailLength",
-				message: "Email is too long",
-			});
-		}
+    const emailRegex = "[a-zA-Z0-9_\\.\\+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-\\.]+";
+    if (!email.match(emailRegex)) {
+      console.error(`AuthController: Login: Invalid email string: ${email}`);
+      return res.json({
+        success: false,
+        errorType: "LoginEmailRegex",
+        message: "Email must be in name@email.com format",
+      });
+    } else if (email.length > 300) {
+      console.error(`AuthController: Login: Invalid email string: ${email}`);
+      return res.json({
+        success: false,
+        errorType: "LoginEmailLength",
+        message: "Email is too long",
+      });
+    }
 
-		if (password.length < 6 || password.length > 50) {
-			console.error(
-				`AuthController: Login: Invalid password string: ${email}`
-			);
-			return res.json({
-				success: false,
-				errorType: "LoginPasswordLength",
-				message: "Password should be within length of 6 - 50.",
-			});
-		}
+    if (password.length < 6 || password.length > 50) {
+      console.error(`AuthController: Login: Invalid password string: ${email}`);
+      return res.json({
+        success: false,
+        errorType: "LoginPasswordLength",
+        message: "Password should be within length of 6 - 50.",
+      });
+    }
 
-		try {
-			const result = await UserServices.Login(email, password);
-			return res.json(result);
-		} catch (err) {
-			return res.json({
-				success: false,
-				errorType: "LoginCatchBlock",
-				message: `${err}`,
-			});
-		}
-	}
+    try {
+      const result = await UserServices.Login(email, password);
+      return res.json(result);
+    } catch (err) {
+      return res.json({
+        success: false,
+        errorType: "LoginCatchBlock",
+        message: `${err}`,
+      });
+    }
+  }
 
-	static async userLoginGetID(req, res) {
-		const { email, password } = req.body;
+  static async userLoginGetID(req, res) {
+    const { email, password } = req.body;
 
-		try {
-			const result = await UserServices.LoginGetID(email, password);
-			return res.json(result);
-		} catch (err) {
-			return res.json({
-				success: false,
-				errorType: "LoginCatchBlock",
-				message: `${err}`,
-			});
-		}
-	}
+    try {
+      const result = await UserServices.LoginGetID(email, password);
+      return res.json(result);
+    } catch (err) {
+      return res.json({
+        success: false,
+        errorType: "LoginCatchBlock",
+        message: `${err}`,
+      });
+    }
+  }
 
+  static async userLogout(req, res) {
+    const userID = req.params.user;
+    try {
+      const result = await UserServices.Logout(userID);
+      return res.json(result);
+    } catch (err) {
+      return res.json({
+        success: false,
+        errorType: "LogoutCatchBlock",
+        message: `${err}`,
+      });
+    }
+  }
 
-	static async userLogout(req, res) {
-		const userID = req.params.user;
-		try {
-			const result = await UserServices.Logout(userID);
-			return res.json(result);
-		} catch (err) {
-			return res.json({
-				success: false,
-				errorType: "LogoutCatchBlock",
-				message: `${err}`,
-			});
-		}
-	}
+  static async editAccountDetails(req, res) {
+    try {
+      const userId = req.body.userId;
+      const username = req.body.username;
+      const password = req.body.password;
+      const email = req.body.email;
+      const gender = req.body.gender;
+      const region = req.body.region;
+      const mtl = req.body.motherTongueLanguage;
+      const eduLevel = req.body.educationLevel;
+      const interests = req.body.ccaInterests;
 
-	static async editAccountDetails(req, res) {
-		try {
-			const userId = req.body.userId;
-			const username = req.body.username;
-			const password = req.body.password;
-			const email = req.body.email;
-			const gender = req.body.gender;
-			const region = req.body.region;
-			const mtl = req.body.motherTongueLanguage;
-			const eduLevel = req.body.educationLevel;
-			const interests = req.body.ccaInterests;
+      const ReviewResponse = await UserAuthDAO.editUser(
+        userId,
+        username,
+        email,
+        password,
+        gender,
+        region,
+        mtl,
+        eduLevel,
+        interests
+      );
+      var { error } = ReviewResponse;
+      if (error) {
+        return res.json({
+          success: false,
+          errorType: "EditCatchBlock",
+          message: `${error}`,
+        });
+      }
 
-			const ReviewResponse = await UserAuthDAO.editUser(
-				userId,
-				username,
-				email,
-				password,
-				gender,
-				region,
-				mtl,
-				eduLevel,
-				interests
-			);
-			var { error } = ReviewResponse;
-			if (error) {
-				return res.json({
-					success: false,
-					errorType: "EditCatchBlock",
-					message: `${error}`,
-				});
-			}
+      if (ReviewResponse.modifiedCount == 0) {
+        return res.json({
+          success: false,
+          errorType: "EditNoChange",
+          message: "Unable to edit account details",
+        });
+      }
 
-			if (ReviewResponse.modifiedCount == 0) {
-				return res.json({
-					success: false,
-					errorType: "EditNoChange",
-					message: "Unable to edit account details",
-				});
-			}
+      return res.json({
+        success: true,
+        message: "Successfully edited account",
+      });
+    } catch (e) {
+      return res.json({
+        success: false,
+        errorType: "EditCatchBlock",
+        message: `${err}`,
+      });
+    }
+  }
 
-			return res.json({
-				success: true,
-				message: "Successfully edited account",
-			});
-		} catch (e) {
-			return res.json({
-				success: false,
-				errorType: "EditCatchBlock",
-				message: `${err}`,
-			});
-		}
-	}
+  static async Refresh(req, res) {
+    const userID = req.params.user;
 
-	static async Refresh(req, res){
-		const userID = req.params.user;
+    try {
+      const result = await UserServices.RefreshToken(userID);
+      res.json(result);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  }
 
-		try {
-			const result = await UserServices.RefreshToken(userID);
-			res.json(result);
-		} catch (err) {
-			res.status(500).json({ message: err.message });
-		}
-	}
-	
-	// static async SetExpoToken(req, res){
-	// 	const userID = req.params.user;
-	// 	const { expoToken } = req.body;
-		
-	// 	if (!expoToken){
-	// 		console.error(`AuthController: Login: Invalid expo token: ${expoToken}`)
-	// 		return res.status(500).json({ message: 'Expo token detaill is null' });
-	// 	}
-		
-	// 	try {
-	// 		const result = await UserServices.SetUserExpoToken(userID, expoToken);
-	// 		res.json(result);
-	// 	} catch (err) {
-	// 		res.status(500).json({ message: err.message });
-	// 	}
-	// }
+  // static async SetExpoToken(req, res){
+  // 	const userID = req.params.user;
+  // 	const { expoToken } = req.body;
+
+  // 	if (!expoToken){
+  // 		console.error(`AuthController: Login: Invalid expo token: ${expoToken}`)
+  // 		return res.status(500).json({ message: 'Expo token detaill is null' });
+  // 	}
+
+  // 	try {
+  // 		const result = await UserServices.SetUserExpoToken(userID, expoToken);
+  // 		res.json(result);
+  // 	} catch (err) {
+  // 		res.status(500).json({ message: err.message });
+  // 	}
+  // }
 }
--- backend/services/users.services.js
+++ backend/services/users.services.js
@@ -1,5 +1,5 @@
 import UserAuthDAO from "../dao/userAuthDAO.js";
-import RefreshTokenRepo from "../dao/refreshTokenRepo.js"
+import RefreshTokenRepo from "../dao/refreshTokenRepo.js";
 
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
@@ -10,278 +10,278 @@ const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || null;
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || null;
 
 export default class UserSevices {
-	static async GenerateGeneralJWTToken(userID) {
-		if (!ACCESS_TOKEN_SECRET) {
-			console.error(
-				"AuthService: GenerateGeneralJWTToken: No JWT Secret"
-			);
-			throw new Error("An error occured while trying to generate token");
-		}
+  static async GenerateGeneralJWTToken(userID) {
+    if (!ACCESS_TOKEN_SECRET) {
+      console.error("AuthService: GenerateGeneralJWTToken: No JWT Secret");
+      throw new Error("An error occured while trying to generate token");
+    }
 
-		try {
-			const token = jwt.sign({userID}, ACCESS_TOKEN_SECRET, { expiresIn: "24h" });
-			return token;
-		} catch (err) {
-			console.error(
-				"AuthService: GenerateGeneralJWTToken: Failed to sign JWT Token"
-			);
-			throw new Error("An error occured while trying to generate token");
-		}
-	}
+    try {
+      const token = jwt.sign({ userID }, ACCESS_TOKEN_SECRET, {
+        expiresIn: "24h",
+      });
+      return token;
+    } catch (err) {
+      console.error(
+        "AuthService: GenerateGeneralJWTToken: Failed to sign JWT Token"
+      );
+      throw new Error("An error occured while trying to generate token");
+    }
+  }
 
-	static async GenerateRefreshJWTToken(userID) {
-		if (!REFRESH_TOKEN_SECRET) {
-			console.error(
-				"AuthService: GenerateRefreshJWTToken: No JWT Secret"
-			);
-			throw new Error(
-				"An error occured while trying to generate refresh token"
-			);
-		}
+  static async GenerateRefreshJWTToken(userID) {
+    if (!REFRESH_TOKEN_SECRET) {
+      console.error("AuthService: GenerateRefreshJWTToken: No JWT Secret");
+      throw new Error(
+        "An error occured while trying to generate refresh token"
+      );
+    }
 
-		try {
-			const token = jwt.sign(
-				{ userID, generatedAt: Date.now() },
-				REFRESH_TOKEN_SECRET,
-				{ expiresIn: "24h" }
-			);
-			return token;
-		} catch (err) {
-			console.error(
-				"AuthService: GenerateRefreshJWTToken: Failed to sign refresh JWT Token"
-			);
-			throw new Error(
-				"An error occured while trying to generate refresh token"
-			);
-		}
-	}
+    try {
+      const token = jwt.sign(
+        { userID, generatedAt: Date.now() },
+        REFRESH_TOKEN_SECRET,
+        { expiresIn: "24h" }
+      );
+      return token;
+    } catch (err) {
+      console.error(
+        "AuthService: GenerateRefreshJWTToken: Failed to sign refresh JWT Token"
+      );
+      throw new Error(
+        "An error occured while trying to generate refresh token"
+      );
+    }
+  }
 
-	static async Register(
-		username,
-		email,
-		password,
-		gender,
-		region,
-		mtl,
-		edulevel,
-		interest
-	) {
-		const existingUser = await UserAuthDAO.GetUserByEmail(email);
+  static async Register(
+    username,
+    email,
+    password,
+    gender,
+    region,
+    mtl,
+    edulevel,
+    interest
+  ) {
+    const existingUser = await UserAuthDAO.GetUserByEmail(email);
 
-		if (existingUser) {
-			console.error(`An account with ${email} has already been created`);
-			return {
-				success: false,
-				errorType: "RegisterAccountExist",
-				message: `An account with ${email} has already been created`,
-			};
-		}
+    if (existingUser) {
+      console.error(`An account with ${email} has already been created`);
+      return {
+        success: false,
+        errorType: "RegisterAccountExist",
+        message: `An account with ${email} has already been created`,
+      };
+    }
 
-		// Hash password
-		let hashedPassword = null;
-		try {
-			hashedPassword = bcrypt.hashSync(password, 10);
-		} catch (err) {
-			console.log(err);
-			console.error(
-				`AuthService: Register: An error occured while trying to hash password for ${email}`
-			);
-			throw new Error(
-				"An error occured while trying to register account"
-			);
-		}
+    // Hash password
+    let hashedPassword = null;
+    try {
+      hashedPassword = bcrypt.hashSync(password, 10);
+    } catch (err) {
+      console.log(err);
+      console.error(
+        `AuthService: Register: An error occured while trying to hash password for ${email}`
+      );
+      throw new Error("An error occured while trying to register account");
+    }
 
-		let newUser = null;
-		try {
-			newUser = await UserAuthDAO.CreateUser(
-				username,
-				email,
-				hashedPassword, // Encrypted password
-				gender,
-				region,
-				mtl,
-				edulevel,
-				interest
-			);
-			// return { success: true, message: "Success" };
-		} catch (err) {
-			console.error(
-				`An error occured while trying to create account ${err}`
-			);
-			return {
-				success: false,
-				errorType: "RegisterCatchBlock",
-				message: `An error occured while trying to create account ${err}`,
-			};
-		}
+    let newUser = null;
+    try {
+      newUser = await UserAuthDAO.CreateUser(
+        username,
+        email,
+        hashedPassword, // Encrypted password
+        gender,
+        region,
+        mtl,
+        edulevel,
+        interest
+      );
+      // return { success: true, message: "Success" };
+    } catch (err) {
+      console.error(`An error occured while trying to create account ${err}`);
+      return {
+        success: false,
+        errorType: "RegisterCatchBlock",
+        message: `An error occured while trying to create account ${err}`,
+      };
+    }
 
-		// Generate tokens
-		try {
-			const user = await UserAuthDAO.GetUserByEmail(email)
-			const { _id, ...others } = user;
-			const generalToken = await this.GenerateGeneralJWTToken(_id.toString());
-			const refreshToken = await this.GenerateRefreshJWTToken(_id.toString());
-			// Sub token for every refresh of token
-			await RefreshTokenRepo.CreateRefreshToken(
-				_id.toString(),
-				generalToken,
-				refreshToken
-			);
+    // Generate tokens
+    try {
+      const user = await UserAuthDAO.GetUserByEmail(email);
+      const { _id, ...others } = user;
+      const generalToken = await this.GenerateGeneralJWTToken(_id.toString());
+      const refreshToken = await this.GenerateRefreshJWTToken(_id.toString());
+      // Sub token for every refresh of token
+      await RefreshTokenRepo.CreateRefreshToken(
+        _id.toString(),
+        generalToken,
+        refreshToken
+      );
 
-			return { 
-				success: true,
-				token: generalToken,
-				refreshToken: refreshToken
-			};
-		} catch (err) {
-			throw new Error(
-				"An error occured while generating authorization tokens"
-			);
-		}
-	}
+      return {
+        success: true,
+        token: generalToken,
+        refreshToken: refreshToken,
+      };
+    } catch (err) {
+      throw new Error("An error occured while generating authorization tokens");
+    }
+  }
 
-	static async Login(email, password) {
-		const user = await UserAuthDAO.GetUserByEmail(email);
-		// If user does not exist
-		if (!user) {
-			console.error(`User Account does not exist ${email}`);
-			return {
-				success: false,
-				errorType: "LoginNoSuchAccount",
-				message: `Invalid Login: User Account does not exist ${email}`,
-			};
-		}
+  static async Login(email, password) {
+    const user = await UserAuthDAO.GetUserByEmail(email);
+    // If user does not exist
+    if (!user) {
+      console.error(`User Account does not exist ${email}`);
+      return {
+        success: false,
+        errorType: "LoginNoSuchAccount",
+        message: `Invalid Login: User Account does not exist ${email}`,
+      };
+    }
 
-		// if (password != user.password) {
-		if (!bcrypt.compareSync(password, user.password)) {
-			console.error(`Failed login for account ${email}`);
-			return {
-				success: false,
-				errorType: "LoginWrongPassword",
-				message: "Invalid Login: Wrong password",
-				token: "",
-				refreshToken: "",
-			};
-		}
+    // if (password != user.password) {
+    if (!bcrypt.compareSync(password, user.password)) {
+      console.error(`Failed login for account ${email}`);
+      return {
+        success: false,
+        errorType: "LoginWrongPassword",
+        message: "Invalid Login: Wrong password",
+        token: "",
+        refreshToken: "",
+      };
+    }
 
-		try {
-			const generalToken = await this.GenerateGeneralJWTToken(user._id.toString());
-			const refreshToken = await this.GenerateRefreshJWTToken(user._id.toString());
-			await RefreshTokenRepo.CreateRefreshToken(user._id.toString(), generalToken, refreshToken);
-			return {
-				user,
-				success: true,
-				message: "Successfully logged in",
-				token: generalToken,
-				refreshToken : refreshToken,
-			};
-		} catch {
-			console.error(
-				`AuthService: Login: Error generating tokens for user ${email}`
-			);
-			throw new Error("An error occured while logging in");
-		}
-	}
+    try {
+      const generalToken = await this.GenerateGeneralJWTToken(
+        user._id.toString()
+      );
+      const refreshToken = await this.GenerateRefreshJWTToken(
+        user._id.toString()
+      );
+      await RefreshTokenRepo.CreateRefreshToken(
+        user._id.toString(),
+        generalToken,
+        refreshToken
+      );
+      return {
+        user,
+        success: true,
+        message: "Successfully logged in",
+        token: generalToken,
+        refreshToken: refreshToken,
+      };
+    } catch {
+      console.error(
+        `AuthService: Login: Error generating tokens for user ${email}`
+      );
+      throw new Error("An error occured while logging in");
+    }
+  }
 
-	static async LoginGetID(email, password) {
-		const user = await UserAuthDAO.GetUserByEmail(email);
-		// If user does not exist
-		if (!user) {
-			console.error(`User Account does not exist ${email}`);
-			return {
-				success: false,
-				errorType: "LoginNoSuchAccount",
-				message: `Invalid Login: User Account does not exist ${email}`,
-			};
-		}
+  static async LoginGetID(email, password) {
+    const user = await UserAuthDAO.GetUserByEmail(email);
+    // If user does not exist
+    if (!user) {
+      console.error(`User Account does not exist ${email}`);
+      return {
+        success: false,
+        errorType: "LoginNoSuchAccount",
+        message: `Invalid Login: User Account does not exist ${email}`,
+      };
+    }
 
-		// Hash password
-		let hashedPassword = null;
-		try {
-			hashedPassword = bcrypt.hashSync(password, 10);
-		} catch (err) {
-			console.log(err);
-			console.error(
-				`AuthService: Register: An error occured while trying to hash password for ${email}`
-			);
-			throw new Error(
-				"An error occured while trying to register account"
-			);
-		}
+    // Hash password
+    let hashedPassword = null;
+    try {
+      hashedPassword = bcrypt.hashSync(password, 10);
+    } catch (err) {
+      console.log(err);
+      console.error(
+        `AuthService: Register: An error occured while trying to hash password for ${email}`
+      );
+      throw new Error("An error occured while trying to register account");
+    }
+    console.log("Hashed: ", hashedPassword);
+    console.log("User Pass: ", user.password);
 
-		if (hashedPassword != user.password) {
-			console.error(`Failed login for account ${email}`);
-			return {
-				success: false,
-				errorType: "LoginWrongPassword",
-				message: "Invalid Login: Wrong password",
-			};
-		}
-		// TODO:
-		const { _id, ...others } = user;
-		return { _id, password, success: true, message: "Successfully logged in" };
-	}
+    if (hashedPassword != user.password) {
+      console.error(`Failed login for account ${email}`);
+      return {
+        success: false,
+        errorType: "LoginWrongPassword",
+        message: "Invalid Login: Wrong password",
+      };
+    }
+    // TODO:
+    const { _id, ...others } = user;
+    return { _id, password, success: true, message: "Successfully logged in" };
+  }
 
-	static async RefreshToken(userID) {
-		if (!userID) {
-			throw new Error("No user ID");
-		}
+  static async RefreshToken(userID) {
+    if (!userID) {
+      throw new Error("No user ID");
+    }
 
-		try {
-			const newToken = GenerateGeneralJWTToken(userID);
-			return { token: newToken };
-		} catch (err) {
-			throw new Error(err.message);
-		}
-	}
+    try {
+      const newToken = GenerateGeneralJWTToken(userID);
+      return { token: newToken };
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
 
-	// static async SetUserExpoToken(userID, expoToken) {
-	// 	if (!userID) {
-	// 		throw new Error("No user ID");
-	// 	}
+  // static async SetUserExpoToken(userID, expoToken) {
+  // 	if (!userID) {
+  // 		throw new Error("No user ID");
+  // 	}
 
-	// 	const user = await UserAuthDAO.GetUserByID(userID)
-	// 	if (!user) {
-	// 		throw new Error(`User of ID ${userID} not found`);
-	// 	}
+  // 	const user = await UserAuthDAO.GetUserByID(userID)
+  // 	if (!user) {
+  // 		throw new Error(`User of ID ${userID} not found`);
+  // 	}
 
-	// 	user.expoToken = expoToken;
-	// 	await user.save();
-	// 	return { message: "Successfully set expo token for user" };
-	// }
+  // 	user.expoToken = expoToken;
+  // 	await user.save();
+  // 	return { message: "Successfully set expo token for user" };
+  // }
 
-	static async Logout(userID) {
-		if (!userID) {
-			console.error("No user ID");
-			return {
-				success: false,
-				errorType: "LogoutIdEmpty",
-				message: "user ID is empty",
-			};
-		}
+  static async Logout(userID) {
+    if (!userID) {
+      console.error("No user ID");
+      return {
+        success: false,
+        errorType: "LogoutIdEmpty",
+        message: "user ID is empty",
+      };
+    }
 
-		const user = await UserAuthDAO.GetUserByID(userID);
-		if (!user) {
-			console.error(`User of ID ${userID} not found`);
-			return {
-				success: false,
-				errorType: "LogoutNoSuchAccount",
-				message: `User of ID ${userID} not found`,
-			};
-		}
+    const user = await UserAuthDAO.GetUserByID(userID);
+    if (!user) {
+      console.error(`User of ID ${userID} not found`);
+      return {
+        success: false,
+        errorType: "LogoutNoSuchAccount",
+        message: `User of ID ${userID} not found`,
+      };
+    }
 
-		try {
-			// user.expoToken = null;
-			// await user.save();
-			await RefreshTokenRepo.DeleteRefreshTokensForUser(userID);
-		} catch (err) {
-			console.error(`AuthService: Logout: ${err}`);
-			return {
-				success: false,
-				errorType: "LogoutCatchBlock",
-				message: `${err}`,
-			};
-		}
-		return { success: true, message: "Successfully logged out" };
-	}
+    try {
+      // user.expoToken = null;
+      // await user.save();
+      await RefreshTokenRepo.DeleteRefreshTokensForUser(userID);
+    } catch (err) {
+      console.error(`AuthService: Logout: ${err}`);
+      return {
+        success: false,
+        errorType: "LogoutCatchBlock",
+        message: `${err}`,
+      };
+    }
+    return { success: true, message: "Successfully logged out" };
+  }
 }
