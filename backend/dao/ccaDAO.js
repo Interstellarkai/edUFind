b4d0bae5d00c86ad0eb6d4941c5e8e896da780f0
KaiSheng Lim <limkaisheng.work@gmail.com>
2022-03-25 01:33:37 +0800

    fix query all

--- backend/dao/ccaDAO.js
+++ backend/dao/ccaDAO.js
@@ -1,106 +1,112 @@
-// DAO - Data Access Object 
-import mongodb from "mongodb"
-const ObjectId = mongodb.ObjectId
-let CCAs // store reference to db 
-
+// DAO - Data Access Object
+import mongodb from "mongodb";
+const ObjectId = mongodb.ObjectId;
+let CCAs; // store reference to db
 
 // export class with several async methods
 export default class CCADAO {
-    // connect to db initially (as our server starts)
-    static async injectDB(conn) {
-        // reference is filled 
-        if (CCAs) {
-            return
-        }
-        // if reference is not filled, we will try to connect with a specific reference in our DB 
-        try {
-            CCAs = await conn.db(process.env.SCHOOLREVIEWS_NS).collection("co_curriculum_activities")
-        } catch (e) {
-            console.error(
-                `Unable to establish a collection handle in CCADAO: ${e}`
-            )
-        }
-    }
+	// connect to db initially (as our server starts)
+	static async injectDB(conn) {
+		// reference is filled
+		if (CCAs) {
+			return;
+		}
+		// if reference is not filled, we will try to connect with a specific reference in our DB
+		try {
+			CCAs = await conn
+				.db(process.env.SCHOOLREVIEWS_NS)
+				.collection("co_curriculum_activities");
+		} catch (e) {
+			console.error(
+				`Unable to establish a collection handle in CCADAO: ${e}`
+			);
+		}
+	}
 
-    // get a list of CCAs in the DB 
-    static async getCCAs({
-        // options 
-        filters = null,
-        // page = 0,
-        // CCAsPerPage = 20,
-    } = {}) {
-        // query
-        let query = {}
-        // filters s
-        if (filters) {
-            if ("school_name" in filters) {
-                // query = { $text: { $search: filters["school_name"] } } // text search, search any word in that text
-                query["school_name"] =  { $eq: filters["school_name"] } // text search, search any word in that text
-            }
-            if ("cca_grouping_desc" in filters) {
-                query["cca_grouping_desc"] =  { $eq: filters["cca_grouping_desc"] }
-            }
-            if ("cca_generic_name" in filters) {
-                query["cca_generic_name"] =  { $eq: filters["cca_generic_name"] }
-            }
-        }
-        
-        let cursor
+	// get a list of CCAs in the DB
+	static async getCCAs({
+		// options
+		filters = null,
+		// page = 0,
+		// CCAsPerPage = 20,
+	} = {}) {
+		// query
+		let query = {};
+		// filters s
+		if (filters) {
+			if ("school_name" in filters) {
+				// query = { $text: { $search: filters["school_name"] } } // text search, search any word in that text
+				query["school_name"] = { $eq: filters["school_name"] }; // text search, search any word in that text
+			}
+			if ("cca_grouping_desc" in filters) {
+				query["cca_grouping_desc"] = {
+					$eq: filters["cca_grouping_desc"],
+				};
+			}
+			if ("cca_generic_name" in filters) {
+				query["cca_generic_name"] = {
+					$eq: filters["cca_generic_name"],
+				};
+			}
+		}
 
-        try {
-            // find all the CCAs from the database that go along with the query that was passed in
-            cursor = await CCAs
-                .find(query)
-        } catch (e) {
-            console.error(`Unable to issue find command, ${e}`)
-            return { CCAsList: [], totalNumCCAs: 0 }
-        }
-        
-        // const displayCursor = cursor.limit(CCAsPerPage).skip(CCAsPerPage * page) // get to a specific page
+		let cursor;
 
-        try {
-            const CCAsList = await displayCursor.toArray()
-            const totalNumCCAs = await CCAs.countDocuments(query)
-            
-            return { CCAsList, totalNumCCAs }
-        } catch (e) {
-            console.error(
-                `Unable to convert cursor to array or problem counting documents, ${e}`
-            )
-            return { CCAsList: [], totalNumCCAs: 0 }
-        }
-    }
+		try {
+			// find all the CCAs from the database that go along with the query that was passed in
+			cursor = await CCAs.find(query);
+		} catch (e) {
+			console.error(`Unable to issue find command, ${e}`);
+			return { CCAsList: [], totalNumCCAs: 0 };
+		}
 
-    static async getSchoolName() {
-        let schoolName = []
-        try {
-            schoolName = await CCAs.distinct("school_name")
-            return schoolName 
-        } catch (e){
-            console.error(`Unable to get school Name,  ${e}`)
-            return schoolName
-        }
-    }
+		const displayCursor = cursor;
+		// .limit(CCAsPerPage)
+		// .skip(CCAsPerPage * page) // get to a specific page
 
-    static async getCcaGenericName() {
-        let ccaGenericName = []
-        try {
-            ccaGenericName = await CCAs.distinct("cca_generic_name")
-            return ccaGenericName 
-        } catch (e){
-            console.error(`Unable to get ccaGenericName,  ${e}`)
-            return ccaGenericName
-        }
-    }
+		try {
+			const CCAsList = await displayCursor.toArray();
+			const totalNumCCAs = await CCAs.countDocuments(query);
 
-    static async getCcaGroupingDesc() {
-        let ccaGroupingDesc = []
-        try {
-            ccaGroupingDesc = await CCAs.distinct("cca_grouping_desc")
-            return ccaGroupingDesc 
-        } catch (e){
-            console.error(`Unable to get CCA Grouping Desc,  ${e}`)
-            return ccaGroupingDesc
-        }
-    }
+			return { CCAsList, totalNumCCAs };
+		} catch (e) {
+			console.error(
+				`Unable to convert cursor to array or problem counting documents, ${e}`
+			);
+			return { CCAsList: [], totalNumCCAs: 0 };
+		}
+	}
+
+	static async getSchoolName() {
+		let schoolName = [];
+		try {
+			schoolName = await CCAs.distinct("school_name");
+			return schoolName;
+		} catch (e) {
+			console.error(`Unable to get school Name,  ${e}`);
+			return schoolName;
+		}
+	}
+
+	static async getCcaGenericName() {
+		let ccaGenericName = [];
+		try {
+			ccaGenericName = await CCAs.distinct("cca_generic_name");
+			return ccaGenericName;
+		} catch (e) {
+			console.error(`Unable to get ccaGenericName,  ${e}`);
+			return ccaGenericName;
+		}
+	}
+
+	static async getCcaGroupingDesc() {
+		let ccaGroupingDesc = [];
+		try {
+			ccaGroupingDesc = await CCAs.distinct("cca_grouping_desc");
+			return ccaGroupingDesc;
+		} catch (e) {
+			console.error(`Unable to get CCA Grouping Desc,  ${e}`);
+			return ccaGroupingDesc;
+		}
+	}
 }
--- backend/dao/moeDAO.js
+++ backend/dao/moeDAO.js
@@ -1,92 +1,97 @@
-// DAO - Data Access Object 
-import mongodb from "mongodb"
-const ObjectId = mongodb.ObjectId
-let MOE // store reference to db 
-
+// DAO - Data Access Object
+import mongodb from "mongodb";
+const ObjectId = mongodb.ObjectId;
+let MOE; // store reference to db
 
 // export class with several async methods
 export default class MOEDAO {
-    // connect to db initially (as our server starts)
-    static async injectDB(conn) {
-        // reference is filled 
-        if (MOE) {
-            return
-        }
-        // if reference is not filled, we will try to connect with a specific reference in our DB 
-        try {
-            MOE = await conn.db(process.env.SCHOOLREVIEWS_NS).collection("moe_programmes")
-        } catch (e) {
-            console.error(
-                `Unable to establish a collection handle in MOEDAO: ${e}`
-            )
-        }
-    }
+	// connect to db initially (as our server starts)
+	static async injectDB(conn) {
+		// reference is filled
+		if (MOE) {
+			return;
+		}
+		// if reference is not filled, we will try to connect with a specific reference in our DB
+		try {
+			MOE = await conn
+				.db(process.env.SCHOOLREVIEWS_NS)
+				.collection("moe_programmes");
+		} catch (e) {
+			console.error(
+				`Unable to establish a collection handle in MOEDAO: ${e}`
+			);
+		}
+	}
 
-    // get a list of MOE in the DB 
-    static async getMOE({
-        // options 
-        filters = null,
-        // page = 0,
-        // moePerPage = 20,
-    } = {}) {
-        // query
-        let query = {}
-        // filters 
-        if (filters) {
-            if ("school_name" in filters) {
-                // query = { $text: { $search: filters["school_name"] } } // text search, search any word in that text
-                query["school_name"] =  { $eq: filters["school_name"] } // text search, search any word in that text
-            }
-            if ("moe_programme_desc" in filters) {
-                query["moe_programme_desc"] =  { $eq: filters["moe_programme_desc"] }
-            }
-        }
-        
-        let cursor
+	// get a list of MOE in the DB
+	static async getMOE({
+		// options
+		filters = null,
+		// page = 0,
+		// moePerPage = 20,
+	} = {}) {
+		// query
+		let query = {};
+		// filters
+		if (filters) {
+			if ("school_name" in filters) {
+				// query = { $text: { $search: filters["school_name"] } } // text search, search any word in that text
+				query["school_name"] = { $eq: filters["school_name"] }; // text search, search any word in that text
+			}
+			if ("moe_programme_desc" in filters) {
+				query["moe_programme_desc"] = {
+					$eq: filters["moe_programme_desc"],
+				};
+			}
+		}
 
-        try {
-            // find all the MOE from the database that go along with the query that was passed in
-            cursor = await MOE
-                .find(query)
-        } catch (e) {
-            console.error(`Unable to issue find command, ${e}`)
-            return { moeList: [], totalNumMOE: 0 }
-        }
+		let cursor;
 
-        // const displayCursor = cursor.limit(moePerPage).skip(moePerPage * page) // get to a specific page
+		try {
+			// find all the MOE from the database that go along with the query that was passed in
+			cursor = await MOE.find(query);
+		} catch (e) {
+			console.error(`Unable to issue find command, ${e}`);
+			return { moeList: [], totalNumMOE: 0 };
+		}
 
-        try {
-            const moeList = await displayCursor.toArray()
-            const totalNumMOE = await MOE.countDocuments(query)
-            
-            return { moeList, totalNumMOE }
-        } catch (e) {
-            console.error(
-                `Unable to convert cursor to array or problem counting documents, ${e}`
-            )
-            return { moeList: [], totalNumMOE: 0 }
-        }
-    }
+		const displayCursor = cursor;
+		// .limit(moePerPage)
+		// .skip(moePerPage * page) // get to a specific page
 
-    static async getSchoolName() {
-        let schoolName = []
-        try {
-            schoolName = await MOE.distinct("school_name")
-            return schoolName 
-        } catch (e){
-            console.error(`Unable to get school name,  ${e}`)
-            return schoolName
-        }
-    }
+		try {
+			// const moeList = await displayCursor.toArray()
+			const moeList = await displayCursor.toArray();
+			const totalNumMOE = await MOE.countDocuments(query);
 
-    static async getMoeProgrammeDesc() {
-        let moeProgrammeDesc = []
-        try {
-            moeProgrammeDesc = await MOE.distinct("moe_programme_desc")
-            return moeProgrammeDesc 
-        } catch (e){
-            console.error(`Unable to get MOE programme desc,  ${e}`)
-            return moeProgrammeDesc
-        }
-    }
+			return { moeList, totalNumMOE };
+		} catch (e) {
+			console.error(
+				`Unable to convert cursor to array or problem counting documents, ${e}`
+			);
+			return { moeList: [], totalNumMOE: 0 };
+		}
+	}
+
+	static async getSchoolName() {
+		let schoolName = [];
+		try {
+			schoolName = await MOE.distinct("school_name");
+			return schoolName;
+		} catch (e) {
+			console.error(`Unable to get school name,  ${e}`);
+			return schoolName;
+		}
+	}
+
+	static async getMoeProgrammeDesc() {
+		let moeProgrammeDesc = [];
+		try {
+			moeProgrammeDesc = await MOE.distinct("moe_programme_desc");
+			return moeProgrammeDesc;
+		} catch (e) {
+			console.error(`Unable to get MOE programme desc,  ${e}`);
+			return moeProgrammeDesc;
+		}
+	}
 }
