21522ae0cf4a2488a27451578354bb7385205cb9
Ka1eidosc0pe <adi.selvam99@gmail.com>
2022-03-31 06:10:03 +0800

    Updates to SchoolProfile Page
    A lot of updates made to SchoolProfilePage, final touches required. Assets uploaded to augment UI of SchoolProfilePage.

--- frontend/src/Pages/SchoolProfilePage.jsx
+++ frontend/src/Pages/SchoolProfilePage.jsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 import Navbar from "../Components/Navbar";
 import { publicRequest } from "../requestMethod";
+import locationLogo from "../Logos/location.png";
+import wwwLogo from "../Logos/www.png";
+import phoneLogo from "../Logos/phone.png";
+import emailLogo from "../Logos/email.png";
+//import { GETSCHOOL } from "../requestMethod"
 
 import CommentSection from "../Components/CommentComponents/CommentSection";
 
@@ -132,17 +137,85 @@ const CommentsForm = styled.form`
 `;
 
 const CommentInput = styled.input`
-  padding: 10px;
-  font-weight: 600;
+    padding: 10px;
+    font-weight: 600;
 
-  font-family: ${(props) =>
-    props.type === "password" && "Verdana, Geneva, Tahoma, sans-serif"};
+    font-family: ${(props) =>
+        props.type === "password" && "Verdana, Geneva, Tahoma, sans-serif"};
 
-  letter-spacing: ${(props) => props.type === "password" && "0.125em"};
+    letter-spacing: ${(props) => props.type === "password" && "0.125em"};
 `;
 
-//const res = await publicRequest.get(GETSCHOOL(schName)); using the API to get school data
+
+//const res = await publicRequest.get(GETSCHOOL(schName)); //using the API to get school data
 // res.data.schools[0].postal_code <- getting the data, and storing it into a variable
+
+// const schoolName = res.data.schools[0].school_name;
+// const schoolAddress = res.data.schools[0].address;
+// const schoolPostalCode = res.data.schools[0].postal_code
+// const schoolWebsite = res.data.schools[0].url_address;
+// const schoolNature = res.data.schools[0].nature_code;
+// const schoolType = res.data.schools[0].type_code;
+// const nearestMRT = res.data.schools[0].mrt_desc;
+// const nearestBus = res.data.schools[0].bus_desc;
+
+/*
+
+const getMTLs = () => {
+    var mtlArray = [];
+
+    const fetchedMTL1 = res.data.schools[0].mothertongue1_code;
+    const fetchedMTL2 = res.data.schools[0].mothertongue2_code;
+    const fetchedMTL3 = res.data.schools[0].mothertongue3_code;
+
+    if (fetchedMTL1 != "na") {
+        mtlArray.push(fetchedMTL1);
+    }
+
+    if (fetchedMTL2 != "na") {
+        mtlArray.push(fetchedMTL2);
+    }
+
+    if (fetchedMTL3 != "na") {
+        mtlArray.push(fetchedMTL3);
+    }
+
+
+}
+
+const getCCAs = () => {
+    var ccaArray = [];
+
+    const fetchedCCA = res.data.schools[0].cca_generic_name;
+
+    for (var i of fetchedCCA) {
+        ccaArray.push(i);
+    }
+
+}
+
+const getElectives = () => {
+    var electivesArray = [];
+
+    const fetchedElectives = res.data.schools[0].moe_programme_desc;
+
+    for (var i of fetchedElectives) {
+        electivesArray.push(i);
+    }
+}
+
+const getSubjects = () => {
+    var subjectsArray = [];
+
+    const fetchedSubjects = res.data.schools[0].subject_desc;
+
+    for (var i of fetchedSubjects) {
+        subjectsArray.push(i);
+    }
+}
+
+
+*/
 
 const SchoolProfilePage = () => {
     return (
@@ -152,12 +225,28 @@ const SchoolProfilePage = () => {
             <SchoolWrapperContainer>
                 
                 <SchoolWrapper>
-                    <SchoolHeader>School Name</SchoolHeader>
-                    <ShortlistButton></ShortlistButton>
-                    <Icon></Icon> {/* Address Icon */}
-                    <BasicInfoText>School Address</BasicInfoText>
-                    <Icon></Icon> {/* Website Icon */}
-                    <BasicInfoText>School Website</BasicInfoText>
+                    <SchoolHeader>School Name</SchoolHeader> {/* insert schoolName here */}
+                    {/* <ShortlistButton></ShortlistButton> */}
+                    
+                    <div style={{ display: 'flex' }}>
+                        <img src={locationLogo} style={{ height: '50px', width: '50px' }} />
+                        <BasicInfoText>School Address</BasicInfoText> {/* insert schoolAddress and schoolPostalCode here and find a way to hyperlink this to google maps! */}
+                    </div>
+
+                    <div style={{ display: 'flex' }}>
+                        <img src={wwwLogo} style={{ height: '50px', width: '50px' }} />
+                        <BasicInfoText>School Website</BasicInfoText> {/* insert schoolWebsite here */}
+                    </div>
+                    
+                    <div style={{ display: 'flex' }}>
+                        <img src={phoneLogo} style={{ height: '50px', width: '50px' }} />
+                        <BasicInfoText>Contact Number</BasicInfoText>
+                    </div>
+                    
+                    <div style={{ display: 'flex' }}>
+                        <img src={emailLogo} style={{ height: '50px', width: '50px' }} />
+                        <BasicInfoText>Email Address</BasicInfoText>
+                    </div>
                 </SchoolWrapper>
 
                 <SchoolWrapper>
@@ -203,28 +292,28 @@ const SchoolProfilePage = () => {
             <AboutWrapperContainer>
                 <AboutWrapper>
                     <AboutHeader>About</AboutHeader>
-                    <AboutText>School Nature:</AboutText>
-                    <AboutText>School Type:</AboutText>
+                    <AboutText>School Nature:</AboutText> {/* insert schoolNature here */}
+                    <AboutText>School Type:</AboutText> {/* insert schoolType here */}
                 </AboutWrapper>
             </AboutWrapperContainer>
 
             <GettingThereWrapperContainer>
                 <GettingThereWrapper>
                     <GettingThereHeader>Getting There</GettingThereHeader>
-                    <GettingThereText>Nearest MRT Station:</GettingThereText>
-                    <GettingThereText>Buses:</GettingThereText>
+                    <GettingThereText>Nearest MRT Station:</GettingThereText> {/* insert nearestMRT here */}
+                    <GettingThereText>Buses:</GettingThereText> {/* insert nearestBus here */}
                 </GettingThereWrapper>
             </GettingThereWrapperContainer>
 
-            <CommentsWrapperContainer>
-                <CommentsWrapper>
-                    <CommentsHeader>Comments</CommentsHeader>
-                    <CommentsForm>
+            {/* <CommentsWrapperContainer> */}
+                {/* <CommentsWrapper> */}
+                    {/* <CommentsHeader>Comments</CommentsHeader> */}
+                    {/* <CommentsForm> */}
                         {/* <CommentInput>Write a comment</CommentInput> */}
-                        <CommentButton>View More Comments</CommentButton>
-                    </CommentsForm>
-                </CommentsWrapper>
-            </CommentsWrapperContainer>
+                        {/* <CommentButton>View More Comments</CommentButton> */}
+                    {/* </CommentsForm> */}
+                {/* </CommentsWrapper> */}
+            {/* </CommentsWrapperContainer> */}
 
             {/* This block of code below is testing implementation of a comments feature */}
             <CommentsWrapperContainer>
