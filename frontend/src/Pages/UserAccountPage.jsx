ee9e51a6736acb62583b94af29c4ef6a1cd0fdcf
Andre Lim <Andre_Lim99@outlook.com>
2022-04-01 15:53:08 +0800

    Updated User Account Page

--- frontend/src/Pages/UserAccountPage.jsx
+++ frontend/src/Pages/UserAccountPage.jsx
@@ -10,38 +10,90 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 const Container = styled.div`
   /* height: 100vh; */
   width: 100vw;
-  // background-color: #ffe7c3;
-  background-color: #004175;
-`;
-
-const WrapperContainer = styled.div`
-  /* height: 80%; */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  /* border: black solid; */
+  /* background-color: #ffe7c3; */
+  /* background-color: #004175; */
 `;
 
 const Wrapper = styled.div`
+  margin: auto;
+  margin-top: 5%;
   display: flex;
-  flex-direction: column;
-  /* align-items: center; */
+  width: 90%;
   justify-content: center;
   /* border: solid blue; */
+  justify-content: space-between;
   height: 100%;
+  box-shadow: rgba(0, 0, 0, 0.15) 5px 5px 20px;
+  padding: 15px;
 `;
 
 const Header1 = styled.h1`
-  font-size: 48px;
+  font-size: 25px;
   // color: #264e70;
-  color: #FFFFFF;
+  color: #f16a59;
 `;
 
 const Header2 = styled.h2`
   font-size: 18px;
+  font-weight: bold;
   // color: #000000;
-  color: #FFFFFF;
+  /* color: #ffffff; */
 `;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  /* box-shadow: rgba(0, 0, 0, 0.15) 5px 5px 2.6px; ; */
+`;
+
+const LeftContainerItem = styled.div`
+  display: flex;
+  /* justify-content: space-between; */
+  /* align-items: flex-end; */
+  margin-bottom: 10px;
+`;
+
+const LeftContainerLabel = styled.div`
+  font-size: 18px;
+  flex: 1;
+  font-weight: bold;
+`;
+const LeftContainerValue = styled.div`
+  flex: 1;
+  border-radius: 5px;
+  /* border: solid 1px; */
+  color: #003892;
+  align-items: center;
+  padding: 5px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  /* box-shadow: rgba(0, 0, 0, 0.15) 5px 5px 2.6px; */
+`;
+const PersonalInfoContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding-left: 20px;
+`;
+const InterestContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+
+  padding-left: 20px;
+`;
+const PasswordContainer = styled.div`
+  /* border: solid black; */
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  /* align-items: center; */
+  padding-left: 20px;
+`;
+
 const CcaItem = styled.li``;
 
 const SpanContainer = styled.div`
@@ -187,72 +239,96 @@ const UserAccountPage = () => {
   return (
     <Container>
       <Navbar />
-      <WrapperContainer>
-        <Wrapper>
-          <Header1>Basic User Information</Header1>
-          <Header2>Name: {currentUser.username}</Header2>
-          <Header2>Gender: {currentUser.gender}</Header2>
-          <Header2>Region: {currentUser.region}</Header2>
-          <Header2>Level of Education: {currentUser.educationLevel}</Header2>
-          <Header2>
-            Mother Tongue Language: {currentUser.motherTongueLanguage}
-          </Header2>
 
-          <Header1>Your Interests</Header1>
-          <Header2>Co-Curricular Activities (CCAs)</Header2>
-          {/* For each CCA Interst in Array - Render*/}
-          {currentUser.ccaInterests.map((cca) => (
-            <CcaItem key={cca}> {cca}</CcaItem>
-          ))}
-
-          <Header1>Account Information</Header1>
-          <Header2>Email: {currentUser.email}</Header2>
-          <Form onSubmit={handleSubmit}>
-            {/* The current password will be checked against what's stored in the database, sort of an authorization to allow changing of pswd */}
-            <SpanContainer>
-              <Label>Enter Current Password: </Label>
-              {currentUserError.error && (
-                <Span>{currentUserError.message}</Span>
-              )}
-            </SpanContainer>
-            <Input
-              type="password"
-              placeholder="Current Password"
-              name="password"
-              onChange={handleChange}
-            />
-            <Label>New Password: </Label>
-            <Input
-              type="password"
-              placeholder="Enter New Password"
-              name="newPassword"
-              onChange={handleChange}
-            />
-            <SpanContainer>
-              <Label>Confirm New Password: </Label>{" "}
-              {passMismatch && <Span>Password Mismatch!</Span>}
-            </SpanContainer>
-            <Input
-              type="password"
-              placeholder="Re-enter New Password"
-              name="confirmNewPassword"
-              onChange={handleChange}
-            />
-            {/* If the input in new password and confirm new password match, the new password is updated in the database */}
-            <FormFooterContainer>
-              <Button>Save Changes</Button>{" "}
-              <Span>
-                {showChangePass && (
-                  <SuccessContainer>
-                    Password Changed{" "}
-                    <CheckCircleIcon sx={{ marginLeft: "10px" }} />
-                  </SuccessContainer>
+      <Wrapper>
+        <Left>
+          <PersonalInfoContainer>
+            <Header1>Personal Information</Header1>
+            <LeftContainerItem>
+              <LeftContainerLabel>Name</LeftContainerLabel>{" "}
+              <LeftContainerValue>{currentUser.username}</LeftContainerValue>
+            </LeftContainerItem>
+            <LeftContainerItem>
+              <LeftContainerLabel>Gender</LeftContainerLabel>{" "}
+              <LeftContainerValue>{currentUser.gender}</LeftContainerValue>
+            </LeftContainerItem>
+            <LeftContainerItem>
+              <LeftContainerLabel>Region</LeftContainerLabel>{" "}
+              <LeftContainerValue>{currentUser.region}</LeftContainerValue>
+            </LeftContainerItem>
+            <LeftContainerItem>
+              <LeftContainerLabel>Level of Education</LeftContainerLabel>{" "}
+              <LeftContainerValue>
+                {currentUser.educationLevel}
+              </LeftContainerValue>
+            </LeftContainerItem>
+            <LeftContainerItem>
+              <LeftContainerLabel>Mother Tongue Language</LeftContainerLabel>{" "}
+              <LeftContainerValue>
+                {currentUser.motherTongueLanguage}
+              </LeftContainerValue>
+            </LeftContainerItem>
+          </PersonalInfoContainer>
+          <InterestContainer>
+            <Header1>Interests</Header1>
+            <Header2>Co-Curricular Activities (CCAs)</Header2>
+            {/* For each CCA Interst in Array - Render*/}
+            {currentUser.ccaInterests.map((cca) => (
+              <CcaItem key={cca}> {cca}</CcaItem>
+            ))}
+          </InterestContainer>
+        </Left>
+        <Right>
+          <PasswordContainer>
+            <Header1>Account Information</Header1>
+            <Header2>Email: {currentUser.email}</Header2>
+            <Form onSubmit={handleSubmit}>
+              {/* The current password will be checked against what's stored in the database, sort of an authorization to allow changing of pswd */}
+              <SpanContainer>
+                <Label>Enter Current Password: </Label>
+                {currentUserError.error && (
+                  <Span>{currentUserError.message}</Span>
                 )}
-              </Span>
-            </FormFooterContainer>
-          </Form>
-        </Wrapper>
-      </WrapperContainer>
+              </SpanContainer>
+              <Input
+                type="password"
+                placeholder="Current Password"
+                name="password"
+                onChange={handleChange}
+              />
+              <Label>New Password: </Label>
+              <Input
+                type="password"
+                placeholder="Enter New Password"
+                name="newPassword"
+                onChange={handleChange}
+              />
+              <SpanContainer>
+                <Label>Confirm New Password: </Label>{" "}
+                {passMismatch && <Span>Password Mismatch!</Span>}
+              </SpanContainer>
+              <Input
+                type="password"
+                placeholder="Re-enter New Password"
+                name="confirmNewPassword"
+                onChange={handleChange}
+              />
+              {/* If the input in new password and confirm new password match, the new password is updated in the database */}
+              <FormFooterContainer>
+                <Button>Save Changes</Button>{" "}
+                <Span>
+                  {showChangePass && (
+                    <SuccessContainer>
+                      Password Changed{" "}
+                      <CheckCircleIcon sx={{ marginLeft: "10px" }} />
+                    </SuccessContainer>
+                  )}
+                </Span>
+              </FormFooterContainer>
+            </Form>
+          </PasswordContainer>
+        </Right>
+      </Wrapper>
     </Container>
   );
 };
